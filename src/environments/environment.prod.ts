29f29f34416ad6c913d72e98e99c0480b6028195
assyaIsk <assyaall69@gmail.com>
2026-06-29 18:05:38 +0500

    restore environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://peijsqmzoekvqzmbpwou.supabase.co',
+  supabaseAnonKey: 'sb_publishable_0EsJWBBMjGSR5vm2ZfD9Gw__QBXO3pj'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://peijsqmzoekvqzmbpwou.supabase.co',
+  supabaseAnonKey: 'sb_publishable_0EsJWBBMjGSR5vm2ZfD9Gw__QBXO3pj'
+};
