5d663e58fa5ca65705037021996567f70763f59c
assyaIsk <assyaall69@gmail.com>
2026-06-29 17:53:40 +0500

    remove environment files from git tracking

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://peijsqmzoekvqzmbpwou.supabase.co',
-  supabaseAnonKey: 'sb_publishable_0EsJWBBMjGSR5vm2ZfD9Gw__QBXO3pj'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://peijsqmzoekvqzmbpwou.supabase.co',
-  supabaseAnonKey: 'sb_publishable_0EsJWBBMjGSR5vm2ZfD9Gw__QBXO3pj'
-};
